f67cf20672f05b23c29b03d6a155e09437c0d622
guil-martin <gu.martin@yahoo.com>
2024-08-21 20:17:26 +0200

    Ignore deploy-branch, updata deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+git checkout develop
+git pull origin develop
+
 # Copy the export folder to the deploy worktree
-cp -r export/* ../deploy-branch/
+rm -rf deploy-branch/*
+cp -r export/* deploy-branch/
 
 # Commit and push to the deploy branch
-cd ../deploy-branch/
+cd deploy-branch/
 git add .
 git commit -m "Deploy latest build"
 git push origin deploy
 
 # Return to the main project directory
 cd -
+
+read -n1 -r -p "Press any key to close..." key
